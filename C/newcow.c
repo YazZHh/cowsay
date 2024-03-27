cdb412a7a920d9a04d91b0e4d7d72e4c27c3f20b
YazZHh <amaury.devalicourt@gmail.com>
2024-03-27 17:12:25 +0100

    affichage vache newcow.c

--- C/newcow.c
+++ C/newcow.c
@@ -1,10 +1,15 @@
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
 void afficher_vache(){
-    "	\ ^	^\ n\
-\
-(00) \\	\ n\
-(   )\\	)\\/\\\ n\
-||---- w |\ n\
-||	||\ n"
+    printf("     \\  ^__^\n");
+    printf("      \\ (oo)\\_______\n");
+    printf("        (__)\\       )\\/\\\n");
+    printf("            ||----w |\n");
+    printf("            ||     ||\n");
+}
+
+int main(){
+    afficher_vache();
 }
