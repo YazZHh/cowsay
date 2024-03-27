09b75b948b9ec2f21c815f0e28330b5d6ecc65aa
YazZHh <amaury.devalicourt@gmail.com>
2024-03-27 21:05:30 +0100

    newcow.c paramètre -e

--- C/newcow.c
+++ C/newcow.c
@@ -1,15 +1,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
-void afficher_vache(){
+void afficher_vache(char* eyes){
     printf("     \\  ^__^\n");
-    printf("      \\ (oo)\\_______\n");
+    printf("      \\ (%c%c)\\_______\n", eyes[0], eyes[1]);
     printf("        (__)\\       )\\/\\\n");
     printf("            ||----w |\n");
     printf("            ||     ||\n");
 }
 
-int main(){
-    afficher_vache();
+int main(int argc, char* argv[]){
+    char eyes[15] = "oo";
+    for (int i=1; i<argc; i++){
+        if (strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--eyes") == 0)
+            strcpy(eyes, argv[i+1]);
+    }
+    afficher_vache(eyes);
 }
