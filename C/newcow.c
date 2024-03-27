d6ccb58cb5edd2a68abdd8217675b869c30605f6
YazZHh <amaury.devalicourt@gmail.com>
2024-03-27 21:35:15 +0100

    option rainbow newcow.c

--- C/newcow.c
+++ C/newcow.c
@@ -3,19 +3,37 @@
 #include <stdio.h>
 #include <string.h>
 
-void afficher_vache(char* eyes){
-    printf("     \\  ^__^\n");
-    printf("      \\ (%c%c)\\_______\n", eyes[0], eyes[1]);
-    printf("        (__)\\       )\\/\\\n");
-    printf("            ||----w |\n");
-    printf("            ||     ||\n");
+void afficher_vache(char* eyes, int rainbow){
+    char *rouge = "\033[0m";
+    char *vert = "\033[0m";
+    char *jaune = "\033[0m";
+    char *cyan = "\033[0m";
+    char *violet = "\033[0m";
+
+    if (rainbow) {
+        rouge = "\033[31m";
+        vert = "\033[32m";
+        jaune = "\033[33m";
+        cyan = "\033[36m";
+        violet = "\033[35m";
+    }
+
+    printf("%s     \\  ^__^\n", rouge);
+    printf("%s      \\ (%c%c)\\_______\n", vert, eyes[0], eyes[1]);
+    printf("%s        (__)\\       )\\/\\\n", jaune);
+    printf("%s            ||----w |\n", cyan);
+    printf("%s            ||     ||%s\n", violet, "\033[0m");
 }
 
 int main(int argc, char* argv[]){
-    char eyes[15] = "oo";
+
+    char eyes[15]="oo";
+    int rainbow=0;
     for (int i=1; i<argc; i++){
         if (strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--eyes") == 0)
             strcpy(eyes, argv[i+1]);
+        else if (strcmp(argv[i], "-r") == 0 || strcmp(argv[i], "--rainbow") == 0)
+            rainbow=1;
     }
-    afficher_vache(eyes);
+    afficher_vache(eyes, rainbow);
 }
