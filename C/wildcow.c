74d0e2fc9eb8a0e68296df51ab028bfa4ab20646
YazZHh <amaury.devalicourt@gmail.com>
2024-03-30 17:07:16 +0100

    Animation wildcow.c finie

--- C/wildcow.c
+++ C/wildcow.c
@@ -53,33 +53,55 @@ void afficher_poteau(int x, int y){
 }
 
 int main(int argc, char* argv[]){
-    update();
-    char eyes[15]="oo";
-    int rainbow=0;
     // for (int i=1; i<argc; i++){
     //     if (strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--eyes") == 0)
     //         strcpy(eyes, argv[i+1]);
     //     else if (strcmp(argv[i], "-r") == 0 || strcmp(argv[i], "--rainbow") == 0)
     //         rainbow=1;
     // }
-    int decalage=50;
-    int mode=0;
-    // for (int i=0; i<16; i++){
-    //     gotoxy(i*i, i);
-    //     printf("O");
-    // }
-    // printf("\n");
 
-    afficher_vache(1, 50, 6, "^^");
-    afficher_poteau(30, 6);
-    while (decalage>-1){
-        afficher_vache(mode, decalage, 6, eyes);
-        afficher_poteau(30, 6);
+    update();
+    char eyes[15]="^^";
+    int abscisse_vache=38;
+    int abscisse_poteau=30;
+    int sol=8;
+    int hauteur_vache=sol;
+    int mode=0;
+
+    // afficher_vache(mode, abscisse_vache, sol, "^^");
+    // afficher_poteau(30, sol);
+    while (abscisse_vache>-1){
+        afficher_vache(mode, abscisse_vache, hauteur_vache, eyes);
+        afficher_poteau(abscisse_poteau, sol);
+
         if (mode == 1)
             mode=0;
         else
             mode=1;
-        decalage--;
+        abscisse_vache--;
+
+        if (abscisse_vache - abscisse_poteau > -3 && abscisse_vache - abscisse_poteau < 5){     // La montée du saut
+            mode=0;
+            hauteur_vache--;
+            strcpy(eyes, "\\/");
+        }
+        else if (abscisse_vache - abscisse_poteau < -11 && abscisse_vache - abscisse_poteau > -19){     // La redescente
+            mode=0;
+            hauteur_vache++;
+            strcpy(eyes, "\\/");
+        }
+        else
+            strcpy(eyes, "^^");
+        
+        if (abscisse_vache - abscisse_poteau >= -11 && abscisse_vache - abscisse_poteau <= -3){
+            mode=0;
+            strcpy(eyes, "00");
+        }
+
+        // if 
+
+
+
         usleep(100000); // On attend 100ms
         update();
 
